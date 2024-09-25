cf218b524b9d6bca975775757cd15bf2314ffff3
Yakuari <diamondarrow360@gmail.com>
2024-09-25 23:01:41 +0800

    OTP

--- dashboard/admin/authentication/admin-class.php
+++ dashboard/admin/authentication/admin-class.php
@@ -1,18 +1,211 @@
 <?php 
     require_once __DIR__. '/../../../database/dbconnection.php';
     include_once __DIR__. '/../../../config/settings-configuration.php';
+    require_once __DIR__.'/../../../src/vendor/autoload.php';
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
 
     class ADMIN
         {
         private $conn;
+        private $settings;
+        private $smtp_email;
+        private $smtp_password;
+
         public function __construct(){
+
+            $this->settings = new SystemConfig();
+            $this->smtp_email = $this->settings->getSmtpEmail();
+            $this->smtp_password = $this->settings->getSmtpPassword();
 
             $database = new Database();
             $this->conn = $database->dbConnection();
         }
 
-        public function addAdmin($csrf_token, $username, $email, $password){
-            $stmt = $this->conn->prepare("SELECT * FROM user WHERE email = :email");
+        public function sendotp($otp,$email){
+            if($email == NULL){
+                echo "<script>alert('No Email found.'); window.location.href = '../../../';</script>";
+                exit;
+            }else {
+                $stmt = $this->runQuery("SELECT * FROM user WHERE email = :email");
+            $stmt->execute(array(":email" => $email));
+            $stmt->fetch(PDO::FETCH_ASSOC);}
+
+            if($stmt->rowCount() > 0){
+                echo "<script>alert('Email already taken. Please try another one'); window.location.href = '../../../';</script>";
+                exit;
+            }else{
+                $_SESSION['OTP'] = $otp;
+                $subject = "OTP VERIFICATION";
+                $message = "
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <meta charset = 'UTF=8'>
+                <title> OTP Verification</title>
+                <style>
+                body {
+                font-family: Arial, sans-serif; 
+                background-color: #fff;
+                margin: 0;
+                padding: 0;
+            }
+                .container{
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 30px;
+                background-color: #ffffff;
+                border-radius: 4px;
+                box-shadow: 2px 4px rgba(0, 0, 0, 0.1);
+            }
+                
+            h1{
+            color: #000;
+            font-size: 24px;
+            margin-bottom: 20px;
+            }
+
+            p{
+            color: #666666;
+            font-size: 16px;
+            margin-bottom: 10px;
+            }
+
+            .button{
+            display: inline-block; 
+            padding: 12px 24px;
+            background-color: #0088cc;
+            color: #ffffff;
+            text-decoration: none;
+            border-radius: 4px;
+            font-size: 16px;
+            margin-top: 20px;
+            }
+
+            .logo {
+            display: block;
+            text-align: center;
+            margin-bottom: 30px;
+            }
+
+            </style>
+            </head>
+            <body>
+            <div class='container'>
+            <div class='logo'>
+            <img src = /// /// ///>
+            </div>
+            <h1> OTP Verification </h1>
+            <p> Hello, $email</p>
+            <p> Your OTP is: $otp</p>
+            <p> If you didn't request an OTP, please ignore this Email.</p>
+            <p>Thank You</p>
+            </div>
+            </body>
+            </html>";
+
+            $this->send_email($email,$message,$subject, $this->smtp_email, $this->smtp_password);
+            echo "<script>alert('We sent the OTP to $email.'); window.location.href = '../../../verify-otp.php';</script>";
+        
+            }
+
+        }
+        public function verifyOTP($username, $password, $otp, $tokencode, $email, $csrf_token){
+            if($otp == $_SESSION['OTP']){
+                unset( $_SESSION['OTP'] );
+
+                $this->addAdmin($csrf_token, $username, $email, $password);
+
+                $subject = "VERIFICATION SUCCES";
+                $message = "
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <meta charset = 'UTF=8'>
+                <title> OTP Verification Succes</title>
+                <style>
+                body {
+                font-family: Arial, sans-serif;
+                background-color: #fff;
+                margin: 0;
+                padding: 0;
+
+            }
+                .container{
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 30px;
+                background-color: #ffffff;
+                border-radius: 4px;
+                box-shadow: • 2px 4px rgba(0, 0, 0, 0.1);
+            }
+
+                
+            h1{
+            color: #000;
+            font-size: 24px;
+            margin-bottom: 20px;
+            }
+
+            p{
+            color: #666666;
+            font-size: 16px;
+            margin-bottom: 10px;
+            }
+
+            .button{
+            display: inline-block; 
+            padding: 12px 24px; 
+            background-color: #8888cc;
+            color: #ffffff;
+            text-decoration: none;
+            border-radius: 4px;
+            font-size: 16px;
+            margin-top: 20px;
+            }
+
+            .logo{
+            display: block;
+            text-align: center;
+            margin-bottom: 30px;
+            }
+            </style>
+            </head>
+            <body>
+            <div class='container'>
+            <div class='logo'>
+            <img src = /// /// ///>
+            </div>
+            <h1> Welcome </h1>
+            <p> Hello, $email</p>
+            <p> Welcome to SYstems</p>
+           
+           <p> If you did not sign up for an account, you can ignore this.</p>
+            </div>
+            </body>
+            </html>";
+
+            $this->send_email($email,$subject,$message, $this->smtp_email, $this->smtp_password);
+            echo "<script>alert('Thank You'); window.location.href = '../../../';</script>";
+
+            unset($_SESSION['not_verify_username']);
+            unset($_SESSION['not_verify_email']);
+            unset($_SESSION['not_verify_password']);
+            }else if ($otp == NULL){
+                echo "<script>alert('No OTP found.'); window.location.href = '../../../verify-otp.php';</script>";
+                exit;
+            }else{
+            echo "<script>alert('It appears that the otp you entered is invalid.'); window.location.href = '../../../verify-otp.php';</script>";
+                exit;
+                 } 
+
+             }
+
+
+            public function addAdmin($csrf_token, $username, $email, $password){
+            $stmt = $this->runQuery("SELECT * FROM user WHERE email = :email");
             $stmt->execute(array(":email" => $email));
 
             if($stmt->rowCount() > 0){
@@ -54,9 +247,11 @@
                 }
                 unset($_SESSION['csrf_token']);
                 
-                $stmt = $this->conn->prepare('SELECT * FROM user WHERE email = :email');
+                $stmt = $this->runQuery('SELECT * FROM user WHERE email = :email');
                 $stmt->execute(array(":email" => $email));
                 $userRow = $stmt->fetch(PDO::FETCH_ASSOC); 
+
+                
 
                 if($stmt->rowCount() == 1 && $userRow['password'] == md5($password)){
                     $activity = "Has Successfully Signed In";
@@ -85,8 +280,26 @@
                     exit;
         }
 
+        function send_email($email, $message, $subject, $smtp_email, $smtp_password)
+        {
+            $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = "tls";
+        $mail->Host = "smtp.gmail.com";
+        $mail->Port = 587;
+        $mail->addAddress($email);
+        $mail->Username = $smtp_email;
+        $mail->Password = $smtp_password;
+        $mail->setFrom($smtp_email, "yuri");
+        $mail->Subject = $subject;
+        $mail->msgHTML($message);
+        $mail->send();
+        }
+
         public function logs($activity, $user_ID){
-            $stmt = $this->conn->prepare("INSERT INTO logs (user_ID, activity) VALUES (:user_ID, :activity)");
+            $stmt = $this->runQuery("INSERT INTO logs (user_ID, activity) VALUES (:user_ID, :activity)");
             $stmt->execute(array(":user_ID" => $user_ID, ":activity" => $activity ));
         }
 
@@ -109,15 +322,35 @@
 
     }
 
-    if(isset($_POST['btn-signup'])){
-        $csrf_token = trim($_POST['csrf_token']);
-        $username = trim($_POST['username']);
-        $email = trim($_POST['email']);
-        $password = trim($_POST['password']);
+    
+
+        if(isset($_POST['btn-signup'])){
+
+        $_SESSION['not_verify_username'] = trim($_POST['username']);
+        $_SESSION['not_verify_email'] = trim($_POST['email']);
+        $_SESSION['not_verify_password'] = trim($_POST['password']);
+       
+        $email = trim ($_POST['email']);
+        $otp = rand(100000, 999999);
         
         $addADMIN = new ADMIN();
-        $addADMIN->addAdmin($csrf_token, $username, $email, $password);
+        $addADMIN->sendOtp($otp, $email);
     }
+
+
+    if(isset($_POST['btn-verify'])){
+        $csrf_token = trim($_POST['csrf_token']);
+        $username =  $_SESSION['not_verify_username'];
+        $email =  $_SESSION['not_verify_email'];
+        $password =  $_SESSION['not_verify_password'];
+
+        $tokencode = md5(uniqid(rand()));
+        $otp = trim($_POST['otp']);
+
+        $adminVerify = new ADMIN();
+       $adminVerify->verifyOTP($username, $password, $otp, $tokencode, $email, $csrf_token);
+    }
+
 
     if(isset($_POST['btn-signin'])){
         $csrf_token = trim($_POST['csrf_token']);
@@ -133,6 +366,8 @@
         $adminSignOut = new ADMIN();
         $adminSignOut-> adminSignout();
     }
+    
+    
    
 
 ?>
