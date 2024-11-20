71c562a5de32f7d3b2e47d8ad7e02b8cc8e2a4b4
tselaux <tselauxmaesismaet@gmail.com>
2024-11-20 17:10:46 +0800

    Updating forgot password and styles2, and adding img to src

--- forgot-password.php
+++ forgot-password.php
@@ -2,16 +2,27 @@
     include_once 'config/settings-configuration.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+    <link rel="stylesheet" href="src/css/styles2.css">
+</head>
+<body>
 <main>
     <div class="wrapper-main">
         <section class="section-default">
             <h1>Reset your password</h1>
-            <p>We will send email to reset your password</p>
+            <p>We will send email for your new password</p>
             <form action="dashboard/authentication-class.php" method="POST">
-                <input type="email" name="email" placeholder="Reset your password" Required>
+                <input type="email" name="email" placeholder="New Password" Required>
                 <br>
-                <button type="submit" name="find_email"> reset your password! </button>
+                <button type="submit" name="find_email"> Reset </button>
             </form>
         </section>
     </div>
 </main>
+</body>
+</html>
