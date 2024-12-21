6a1088de2e89887fd9f7b45c9bb83f07d0873e82
Jomarii20 <2022312235@dhvsu.edu.ph>
2024-12-21 13:02:41 +0800

    Add files via upload

--- program/bodybuilding.php
+++ program/bodybuilding.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bodybuilding Program</title>
     <link rel="stylesheet" href="../src/css/bodybuilding.css">
-    
-    
+
+
 </head>
 
 <body>
@@ -25,10 +25,6 @@
                     <li><a href="../index.php#booking">Booking</a></li>
                     <li><a href="../index.php#about-us">About Us</a></li>
                 </ul>
-                <div class="nav-buttons">
-                    <button id="show-login">Sign in</button>
-                    <button id="show-signup">Sign Up</button>
-                </div>
                 <button class="menu-button" onclick="toggleSidebar()">☰</button>
             </div>
         </nav>
@@ -36,62 +32,14 @@
 
     <!-- Sidebar -->
     <ul class="sidebar">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#program">Program</a></li>
-            <li><a href="#subscription">Subscription</a></li>
-            <li><a href="#booking">Booking</a></li>
-            <li><a href="#about-us">About Us</a></li>
-            <button class="sidebar-close-btn">&times;</button>
-        </ul>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="../index.php#program">Program</a></li>
+        <li><a href="../index.php#subscription">Subscription</a></li>
+        <li><a href="./index.php#booking">Booking</a></li>
+        <li><a href="../index.php#about-us">About Us</a></li>
+        <button class="sidebar-close-btn">&times;</button>
+    </ul>
 
-    <!-- Login Popup -->
-    <div class="popup login-popup">
-        <div class="close-btn">&times;</div>
-        <div class="form">
-            <form action="config/system_config_signIn.php" method="POST">
-                <h2>Sign In</h2>
-                <div class="form-element">
-                    <input type="email" name="email" placeholder="Email" required>
-                </div>
-                <div class="form-element">
-                    <input type="password" name="password" placeholder="Password" required>
-                </div>
-                <button type="submit" name="submit">Sign In</button>
-                <div class="form-element">
-                    <a href="forgot-password.php">FORGOT PASSWORD?</a>
-                </div>
-            </form>
-        </div>
-    </div>
-
-    <!-- Signup Popup -->
-    <div class="popup signup-popup">
-        <div class="close-btn">&times;</div>
-        <div class="form">
-            <form action="config/system_config_signup.php" method="POST">
-                <h2>Register Here</h2>
-                <div class="form-element">
-                    <input type="text" name="uid" placeholder="Username" required>
-                </div>
-                <div class="form-element">
-                    <input type="email" name="email" placeholder="Email" required>
-                </div>
-                <div class="form-element">
-                    <input type="password" name="password" placeholder="Password" required>
-                    <select name="type">
-                        <option value="manager">Manager</option>
-                        <option value="user">User</option>
-                        <option value="admin">Admin</option>
-                    </select><br>
-                </div>
-                <div class="form-element">
-                    <label for="months">How many Months:</label>
-                    <input type="number" name="months" placeholder="Enter how many months" min="1" required>
-                </div>
-                <button type="submit" name="submit">Sign Up</button>
-            </form>
-        </div>
-    </div>
 
     <div class="body-title">
         <h1>BODY BUILDING PROGRAM</h1>
